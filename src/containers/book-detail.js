c07c8531f799a30a7fb810efadf41300754e28ea
hagnus <adrianomfalcao@gmail.com>
2017-10-16 13:58:54 -0300

    Simple react example(Static)

---
+++ src/containers/book-detail.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Component } from "react";
+import { connect } from 'react-redux';
+
+class BookDetail extends Component {
+    render() {
+        if (!this.props.book) {
+            return <div>Select a book to get started!</div>
+        }
+
+        return (
+            <div>
+                <h3>{this.props.book.title}</h3>
+                <div>
+                    Pages: {this.props.book.pages}
+                </div>
+            </div>
+        )
+    }
+};
+
+function mapStateToProps(state) {
+    return {
+        book: state.activeBook
+    };
+}
+
+export default connect(mapStateToProps) (BookDetail);
---
+++ src/reducers/books-reducer.js
@@ -0,0 +1,11 @@
+
+export default function() {
+    return [
+        {title: "Aurelio Mertz Jr." ,pages: 347},
+        {title: "Daron Schinner"    ,pages: 25},
+        {title: "Laurie MacGyver"   ,pages: 123},
+        {title: "Jedediah Johnson"  ,pages: 32},
+        {title: "Elda Blanda"       ,pages: 56},
+        {title: "Tito Kunze PhD"    ,pages: 431}
+    ]
+}
